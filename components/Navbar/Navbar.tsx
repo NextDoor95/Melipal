f5d950cc19ffd44adbc22ad425ef14fa74347b6b
NextDoor <nestorhuircapan25@gmail.com>
2024-10-27 08:24:48 -0300

    Added burguer button
    Added burguer button to mobile version, still need some changes but I don't have more time today

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,8 +1,12 @@
 "use client"
 
+import { Size } from '../types'
+import { Slant as Hamburger } from 'hamburger-react'
+
 import Link from 'next/link'
 import { motion } from "framer-motion";
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
 import { useTranslation } from 'react-i18next';
 import Translation from '../ui/Translation';
 
@@ -18,8 +22,43 @@ function Navbar() {
     { text: t('navbar.videos'), href: "/" }
   ]
 
+  const size: Size = useWindowSize()
+  const [isMobileNavbarVisible, setIsMobileNavbarVisible] = useState(false)
+
+  function useWindowSize(): Size {
+		// Initialize state with undefined width/height so server and client renders match
+		// Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+		const [windowSize, setWindowSize] = useState<Size>({
+			width: undefined,
+			height: undefined,
+		})
+		useEffect(() => {
+			// Handler to call on window resize
+			function handleResize() {
+				// Set window width/height to state
+				setWindowSize({
+					width: window.innerWidth,
+					height: window.innerHeight,
+				})
+			}
+			// Add event listener
+			window.addEventListener('resize', handleResize)
+			// Call handler right away so state gets updated with initial window size
+			handleResize()
+			// Remove event listener on cleanup
+			return () => window.removeEventListener('resize', handleResize)
+		}, []) // Empty array ensures that effect is only run on mount
+		return windowSize
+	}
+
     return (
     <nav className="navbar">
+      {size.width! <= 900 ? (
+						<Hamburger
+							toggled={isMobileNavbarVisible}
+							toggle={setIsMobileNavbarVisible}
+						/>
+			) : (
       <ol className="navList">
         {navItems.map((item, index) => (
           <li key={item.text} className="navItem">
@@ -52,6 +91,24 @@ function Navbar() {
         ))}
         <Translation />
       </ol>
+      )}
+      {isMobileNavbarVisible && (
+
+        <div className="navbar--mobile">
+            <Link href="/">
+            historia
+            </Link>
+            <Link href="/">
+            fragmentos
+            </Link>
+            <Link href="/">
+            videos
+            </Link>
+            <div>
+              <Translation />
+            </div>
+        </div>
+      )}
       
     </nav>
   )
